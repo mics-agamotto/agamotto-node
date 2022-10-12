d87c80fa0b5eac4f4f3b2436317a9511242587ae
Steven Yue <steven.yue@sandboxquantum.com>
2022-10-12 16:17:31 +0000

    Add ssl support in prod

--- src/agamotto/__main__.py
+++ src/agamotto/__main__.py
@@ -5,5 +5,12 @@ def main():
     app.run(host="0.0.0.0", port=8000, workers=1, dev=True)
 
 
+def main_prod():
+    app.run(host="0.0.0.0", port=443, ssl=dict(
+    cert="/certs/server.crt",
+    key="/certs/server.key",
+))
+
+
 if __name__ == "__main__":
     main()
