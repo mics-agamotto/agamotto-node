ccd4469c43ee63a4f88a14ea05234c70e9537593
Steven Yue <steven.yue@sandboxquantum.com>
2022-11-15 21:35:39 +0000

    router

--- src/agamotto/example_emails.py
+++ src/agamotto/example_emails.py
@@ -31,3 +31,12 @@ EXAMPLE_EMAILS = [
     "bahwi@me.com",
     "doche@live.com",
 ]
+
+# Example salt: deadbeef123123
+EXAMPLE_PASSWORDS = [
+    "0769aa760a97acbde23496ad9b1f56eba1110dc80e637353dfe35d6801a9f844",  # 123456
+    "d14c7bc07019bf630bcf203cbd16cfc5607d6b3618485ad3772ea8c5ed70dee0",  # password
+    "09e8bc7d2645631f8a96cd50baf4e4407aa1a6de28adcaa48ae2911a16eda4a1",  # berkeley
+    "9cb513d634383e9ffd47dd6709457ab9929450f2d69dcccb4e3d7cfdb3d5370a",  # mics2022
+    "e5ad970e3158c7f1ee0fadfe629eb99b6bad043d15f0357abd955105673da39a",  # agamotto
+]
--- src/agamotto/server.py
+++ src/agamotto/server.py
@@ -2,18 +2,22 @@ import json
 from sanic import Sanic, response
 from sanic.log import logger
 from sanic.response import text, file
-from .example_emails import EXAMPLE_EMAILS
+from .example_emails import EXAMPLE_EMAILS, EXAMPLE_PASSWORDS
 from PSI.server import server_perform_preprocess, server_perform_psi
-from PSI.username_gen import preprocess_email
+from PSI.username_gen import encode_ascii_str, preprocess_email
 
 app = Sanic("agamotto-node")
 
 
 async def preprocess(app):
     logger.info("Preprocessing server set and generating polynomials...")
-    logger.info(f"Server set: {EXAMPLE_EMAILS}")
+    logger.info(f"Server set: {EXAMPLE_EMAILS} {EXAMPLE_PASSWORDS}")
     preprocessed_emails = list(map(preprocess_email, EXAMPLE_EMAILS))
     app.ctx.server_preprocessed = server_perform_preprocess(preprocessed_emails)
+    preprocessed_passwords = list(map(encode_ascii_str, EXAMPLE_PASSWORDS))
+    app.ctx.server_preprocessed_passwords = server_perform_preprocess(
+        preprocessed_passwords
+    )
     logger.info("Preprocessing complete.")
 
 
@@ -30,8 +34,15 @@ async def snapshot(request):
     return text("yes")
 
 
-@app.post("/test")
-async def test(request):
+@app.post("/usernamePSI")
+async def usernamePSI(request):
     res = json.loads(request.body)
     performed_psi = server_perform_psi(app.ctx.server_preprocessed, res)
+    return text(performed_psi)
+
+
+@app.post("/passwordPSI")
+async def passwordPSI(request):
+    res = json.loads(request.body)
+    performed_psi = server_perform_psi(app.ctx.server_preprocessed_passwords, res)
     return text(performed_psi)
